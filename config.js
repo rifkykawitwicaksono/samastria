0f863880d2c05b274ec87f07e0e62a701c730a8c
rifkykawitwicaksono <rifkykawitwicaksono@gmail.com>
2026-09-14 19:04:48 +0700

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // TokoSehat - konfigurasi Supabase
 // GANTI 2 nilai di bawah ini dengan Project URL dan Publishable/anon key dari Supabase.
 // Aman menaruh anon/publishable key di GitHub Pages. JANGAN masukkan service_role key.
-const TOKOSEHAT_SUPABASE_URL = "https://supabase.co";
+const TOKOSEHAT_SUPABASE_URL = "https://appxgwliqdmnqrsdywh.supabase.co";
 const TOKOSEHAT_SUPABASE_ANON_KEY = "sb_publishable_ykh45BuWu3hgnzANq5cgiQ_AJkOGIUa";
 // Tambahan jika library Supabase membutuhkan inisialisasi langsung di file ini:
 const supabase = supabase.createClient(TOKOSEHAT_SUPABASE_URL, TOKOSEHAT_SUPABASE_ANON_KEY);
