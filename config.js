8ee3c1d3378d9dd1976b368f4547d1c24c327f21
rifkykawitwicaksono <rifkykawitwicaksono@gmail.com>
2026-09-16 11:08:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// TokoSehat - konfigurasi Supabase
+const TOKOSEHAT_SUPABASE_URL = "https://appxgwliqdmnqorsdywh.supabase.co";
+const TOKOSEHAT_SUPABASE_ANON_KEY = "sb_publishable_ykh45BuWu3hgnzANq5cgiQ_AJkOGIUa";
