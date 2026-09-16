938fe680fb773ca48767918acdd5e1d2cf08c500
rifkykawitwicaksono <rifkykawitwicaksono@gmail.com>
2026-09-16 12:40:28 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// TokoSehat - konfigurasi Supabase
-const TOKOSEHAT_SUPABASE_URL = "https://appxgwliqdmnqorsdywh.supabase.co";
-const TOKOSEHAT_SUPABASE_ANON_KEY = "sb_publishable_ykh45BuWu3hgnzANq5cgiQ_AJkOGIUa";
